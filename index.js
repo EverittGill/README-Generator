bd130e46e68529c3d9b32360cd06e3544ff4e283
EverittGill <116043918+EverittGill@users.noreply.github.com>
2023-03-24 11:29:09 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+// const path = reqiure('path');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        name: 'username',
+        type: 'input',
+        message: 'What is your name?'
+    }, 
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
